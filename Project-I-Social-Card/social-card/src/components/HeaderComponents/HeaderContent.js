8a3187169c85861be50e12f8015d82501ee5d1de
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-05-28 21:41:22 -0400

    Staging

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContent() {}
+
+export default HeaderContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Header.css';
+
+function ImageThumbnail() {}
+
+export default ImageThumbnail;
