3c7718dbdb828adb3322624778bb0b7d911121b3
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-05-29 21:42:55 -0400

    further improvements, need to stretch

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function Footer() {
-  return (
-    <div class='footer'>
-      <p>Placeholder</p>
-    </div>
-  );
+  return null;
 }
 
 export default Footer;
